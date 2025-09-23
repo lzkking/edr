bd6cf92fa07c296cc3cf0324b5da0a7a7fd0bceb
longzekun-jk <longzekun-jk@360shuke.com>
2025-09-23 11:09:29 +0800

    配置插件共用日志机制

--- plugins/lib/go.mod
+++ plugins/lib/go.mod
@@ -2,4 +2,9 @@ module github.com/lzkking/edr/plugins/lib
 
 go 1.24.6
 
-require google.golang.org/protobuf v1.36.8 // indirect
+require (
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	google.golang.org/protobuf v1.36.8 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+)
---
+++ plugins/lib/log/logger.go
@@ -0,0 +1,49 @@
+package log
+
+import (
+	plugins "github.com/lzkking/edr/plugins/lib"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"strconv"
+	"time"
+)
+
+type Config struct {
+	MaxSize     int
+	Path        string
+	FileLevel   zapcore.LevelEnabler
+	RemoteLevel zapcore.LevelEnabler
+	MaxBackups  int
+	Compress    bool
+	Client      *plugins.Client
+}
+
+func New(cfg Config) (l *zap.Logger) {
+	//	设置远程日志的结构
+	remoteConfig := zap.NewProductionEncoderConfig()
+	remoteConfig.CallerKey = "source"
+	remoteConfig.TimeKey = "timestamp"
+	remoteConfig.EncodeTime = func(t time.Time, z zapcore.PrimitiveArrayEncoder) {
+		z.AppendString(strconv.FormatInt(t.Unix(), 10))
+	}
+	remoteEncoder := zapcore.NewJSONEncoder(remoteConfig)
+	remoteWriter := &remoteWriter{client: cfg.Client}
+	fileEncoder := zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
+	fileWriter := zapcore.AddSync(&lumberjack.Logger{
+		Filename:   cfg.Path,
+		MaxSize:    cfg.MaxSize,
+		MaxBackups: cfg.MaxBackups,
+		Compress:   cfg.Compress,
+	})
+
+	core := zapcore.NewTee(
+		zapcore.NewSamplerWithOptions(
+			zapcore.NewCore(remoteEncoder, remoteWriter, cfg.RemoteLevel), time.Second, 4, 1),
+		zapcore.NewSamplerWithOptions(
+			zapcore.NewCore(fileEncoder, fileWriter, cfg.FileLevel), time.Second, 4, 1),
+	)
+
+	l = zap.New(core, zap.AddCaller())
+	return l
+}
